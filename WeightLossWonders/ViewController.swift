dc75ceb57a40ca25519453f12f30b0809c67f564
Jason <jasonyu112@gmail.com>
2024-02-07 16:15:58 -0800

    adding navigation bar and configure healthkit

--- WeightLossWonders/ViewController.swift
+++ WeightLossWonders/ViewController.swift
@@ -6,14 +6,24 @@
 //
 
 import UIKit
+import HealthKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        if(HKHealthStore.isHealthDataAvailable()){
+            let healthStore = HKHealthStore();
+            let typestoRead = Set([HKObjectType.categoryType(forIdentifier: HKCategoryTypeIdentifier.sleepAnalysis)!])
+            let typestoShare = Set([HKObjectType.categoryType(forIdentifier: HKCategoryTypeIdentifier.sleepAnalysis)!])
+            healthStore.requestAuthorization(toShare: typestoShare, read: typestoRead) { (success, error) -> Void in
+                if success == false{
+                    NSLog("NOT ALLOWED")
+                }
+            }
+            
+        }
     }
-
-
 }
 
