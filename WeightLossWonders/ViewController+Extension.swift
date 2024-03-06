45de27e7b8e0419ac2e4033a73d66c9d44bbf5d6
Inaki Agustin <inakiagustin18@gmail.com>
2024-03-05 20:20:47 -0800

    Separated code that initializes text fields to avoid overcrowding ViewController file

--- WeightLossWonders/ViewController+Extension.swift
+++ WeightLossWonders/ViewController+Extension.swift
@@ -6,3 +6,48 @@
 //
 
 import Foundation
+import UIKit
+
+extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        switch pickerView.tag {
+        case 1:
+            return self.weeklyGoalOptions.count
+        case 2:
+            return self.sexOptions.count
+        default:
+            return 1
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        switch pickerView.tag {
+        case 1:
+            return self.weeklyGoalOptions[row]
+        case 2:
+            return self.sexOptions[row]
+        default:
+            return "Data NOT found"
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        switch pickerView.tag {
+        case 1:
+            self.weeklyGoalTextField.text = self.weeklyGoalOptions[row]
+            self.weeklyGoalTextField.resignFirstResponder()
+        case 2:
+            self.sexTextField.text = self.sexOptions[row]
+            self.sexTextField.resignFirstResponder()
+        default:
+            return
+        }
+    }
+    
+}
+
+
