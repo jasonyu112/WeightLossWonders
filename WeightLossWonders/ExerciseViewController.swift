c8ae39ae89a7ed9456a9fb6ed0832088fee6caa5
Inaki Agustin <inakiagustin18@gmail.com>
2024-03-10 13:38:54 -0700

    Added functionality to fetch activeEnergyBurned from Healthkit API
    Set number of calories to burn goal to be based of option selected in Weekly Goal text field

--- WeightLossWonders/ExerciseViewController.swift
+++ WeightLossWonders/ExerciseViewController.swift
@@ -6,33 +6,105 @@
 //
 
 import UIKit
+import HealthKit
 
 class ExerciseViewController: UIViewController, DataDelegate {
-    var userBMR: Double?
+    var userWeeklyGoal: String?
+    var remainingCaloriesToBurn: Double?
+    var activeEnergyBurned: Double?
+    
+    let healthStore = HKHealthStore()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        loadHKstore()
         
         // Should display "Need to enter values for all entry fields before receiving exercise recommendations
+        getActiveCaloriesBurned()
     }
     
 //    override func viewWillAppear(_ animated: Bool) {
 //        
 //    }
     
-    // Delegate method
-    func sendData(data: String) {
-        self.userBMR = Double(data)
-        print("(Exercise View) User BMR: \(self.userBMR)")
+    // Receives data sent from Profile page
+    func sendToExercise(data: String) {
+        print("received")
+        self.userWeeklyGoal = data
+//        print("(Exercise View) User BMR: \(self.userBMR)")
+        getActiveCaloriesBurned()
     }
     
-    //load number of calories burned from healthkit api
+    func loadHKstore() {
+        let allTypes: Set = [
+            HKQuantityType.workoutType(),
+            HKQuantityType(.activeEnergyBurned),
+            HKQuantityType(.distanceCycling),
+            HKQuantityType(.distanceWalkingRunning),
+            HKQuantityType(.distanceWheelchair),
+            HKQuantityType(.heartRate)
+        ]
+        
+        // Checks that Health data is available on the device.
+        if HKHealthStore.isHealthDataAvailable() {
+            
+            // Requests authorization to the data.
+            healthStore.requestAuthorization(toShare: allTypes, read: allTypes) { (success, error) -> Void in
+                if success == false{
+                    NSLog("NOT ALLOWED")
+                }
+            }
+        }
+    }
     
     
-    func calculateNumCaloriesToBurn(weightLossOption:String){
-        let baseActivityBMR = self.userBMR! * 1.2
+    // Fetches number of active calories currently burned by user from healthkit api
+    func getActiveCaloriesBurned() {
+        let activeEnergyType = HKQuantityType.quantityType(forIdentifier: .activeEnergyBurned)!
+        
+        let now = Date()
+        let startOfDay = Calendar.current.startOfDay(for: now)
+        let predicate = HKQuery.predicateForSamples(withStart: startOfDay, end: now, options: .strictStartDate)
+//        print("now: \(now)")
+//        print("startOfDay: \(startOfDay)")
+        
+        let queryActiveEnergy = HKStatisticsQuery(quantityType: activeEnergyType, quantitySamplePredicate: predicate, options: .cumulativeSum) { (_, results, _) in
+            guard let statistics = results else {
+                return
+            }
+            
+            if let sum = results!.sumQuantity() {
+                self.activeEnergyBurned = sum.doubleValue(for: HKUnit.largeCalorie())
+                print("active energy burned: \(self.activeEnergyBurned)")
+            }
+        }
+        healthStore.execute(queryActiveEnergy)
+    }
+    
+    func setNumCaloriesToBurn(){
+        switch self.userWeeklyGoal! {
+            case "Maintain Weight":
+                self.remainingCaloriesToBurn = 0
+            case "Mild Weight Loss (0.5 lb/week)":
+                self.remainingCaloriesToBurn = 250
+            case "Weight Loss (1 lb/week)":
+                self.remainingCaloriesToBurn = 500
+            case "Extreme Weight Loss (2 lbs/week)":
+                self.remainingCaloriesToBurn = 1000
+            default:
+                self.remainingCaloriesToBurn = 0
+        }
+        
+        // Uses the sedentary or lightly active factor as as my "base" TDEE
+//        let baseTDEE = self.userBMR! * 1.2
+        // Adds in the amount of calories you actually burned through exercise
 //        let finalActivityBMR = baseActivityBMR + caloriesBurned
+    }
+    
+    func setRemainingCaloriesToBurn() {
+        
     }
 }
